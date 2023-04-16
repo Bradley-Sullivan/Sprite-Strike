5ce6a4676b4528e13d5f78489d2f3291e7eeeae4
Bradley-Sullivan <bradleysullivan@nevada.unr.edu>
2023-04-16 00:21:35 -0700

    added sound quips/sfx, refined game loop, added level and pathing, and probably more

--- sketch_apr15a/engine.ino
+++ sketch_apr15a/engine.ino
@@ -1,20 +1,23 @@
-void init_game() {
+void init_game(uint8_t lvl) {
   game_data.g_state = PLAYING;
 
-  player.sprite = 0x0660;
-  player.emitter = 0x0200;
+  player.sprite = 0;
+  player.emitter = 0;
   player.r_pos = 0;
   player.c_pos = 0;
+
   player.atk_timer.interval = P_ATK_CD;
-  player.super_timer.interval = P_ATK_CD;
-
   player.atk_timer.i_time = millis();
-  player.super_timer.i_time = millis();
 
-	spawn_enemies(0);
+	spawn_enemies(lvl);
 
   game_data.p_proj_timer.interval = P_PROJ_INTV;
   game_data.e_proj_timer.interval = E_PROJ_INTV;
+
+  for (int i = 0; i < GAME_HEIGHT; i += 1) {
+    game_data.e_proj[i] = 0;
+    game_data.p_proj[i] = 0;
+  }
 
   game_data.g_state = INIT;
 }
@@ -53,70 +56,115 @@ void create_character() {
   uint8_t done = 0;
   uint8_t create_frame[8];
   uint8_t curs_row = 2, curs_col = 2, s_bits_left = 8, e_bits_left = 2;
-  uint16_t sprite = 0, emitter = 0;
+  uint8_t prev_input = 0;
+  uint16_t eq_count = 0;
 
-  static uint8_t prev_input = 0;
-  static uint16_t eq_count = 0;
+  static uint16_t sprite = 0, emitter = 0;
+  if (game_data.cur_level == 0) {
+    for (int i = 0; i < 8; i += 1) {
+      create_frame[i] = 0;
+      if (i == 1 || i == 6) create_frame[i] = 0x7E;
+      else if (i != 0 && i != 7) create_frame[i] = 0x42;
+    }
 
-  for (int i = 0; i < 8; i += 1) {
-    create_frame[i] = 0;
-    if (i == 1 || i == 6) create_frame[i] = 0x7E;
-    else if (i != 0 && i != 7) create_frame[i] = 0x42;
-  }
+    quip.say(create);
+    quip.say(spa_YOUR);
+    quip.say(spa_CHARACTER);
 
-  while (!done) {
-    // can potentially debounce PIN_B before storing
-    game_data.input = *PIN_B;
+    while (!done) {
+      // can potentially debounce PIN_B before storing
+      game_data.input = *PIN_B;
 
-    if (prev_input != game_data.input || (eq_count > 100 && eq_count < 1000)) {
-      if (prev_input == game_data.input) {
-        delay(300);
-        eq_count = (eq_count == 1000) ? 0 : eq_count + 1;
-      } else {
-        eq_count = 0;
-      }
-
-      if (game_data.input & A_BTN) {
-        if (s_bits_left) {
-          create_frame[curs_row] |= (1 << curs_col);
-          sprite |= (1 << curs_col - 2) << ((curs_row - 2) * SPRITE_WIDTH);
-          s_bits_left -= 1;
-        } else if (e_bits_left) {
-          emitter |= (1 << curs_col - 2) << ((curs_row - 2) * SPRITE_WIDTH);
-          e_bits_left -= 1;
+      if (prev_input != game_data.input || (eq_count > 100 && eq_count < 500)) {
+        if (prev_input == game_data.input) {
+          delay(30);
+          eq_count = (eq_count == 500) ? 0 : eq_count + 1;
         } else {
-          done = 1;
+          eq_count = 0;
         }
-      } if (game_data.input & B_BTN) {
-        if (e_bits_left < 2) done = 1;
-      } if (game_data.input & U_BTN) {
-        if (curs_row > 1) curs_row -= 1;
-      } if (game_data.input & D_BTN) {
-        if (curs_row < 6) curs_row += 1;
-      } if (game_data.input & R_BTN) {
-        if (curs_col > 1) curs_col -= 1;
-      } if (game_data.input & L_BTN) {
-        if (curs_col < 6) curs_col += 1;
-      }
-    } else {
-      eq_count = (eq_count == 1000) ? 0 : eq_count + 1;
-    }
 
-    disp.control(MD_MAX72XX::UPDATE, MD_MAX72XX::OFF);
-    disp.clear();
-    for (int i = 0; i < GAME_HEIGHT; i += 1) {
-      disp.setRow(2, i, (uint8_t)(0x55 >> (i % 2)));
-      disp.setRow(1, i, create_frame[i]);
-      disp.setRow(1, curs_row, (uint8_t)(1 << curs_col) | 0x42 | create_frame[curs_row]);
-      disp.setRow(0, i, (uint8_t)(0x55 >> (i % 2)));      
+        if (game_data.input & A_BTN) {
+          if (s_bits_left) {
+            create_frame[curs_row] |= (1 << curs_col);
+            sprite |= (1 << curs_col - 2) << ((SPRITE_HEIGHT - (curs_row - 2) - 1) * SPRITE_WIDTH);
+            s_bits_left -= 1;
+          } else if (e_bits_left) {
+            emitter |= (1 << curs_col - 2) << ((SPRITE_HEIGHT - (curs_row - 2) - 1) * SPRITE_WIDTH);
+            e_bits_left -= 1;
+          } else {
+            done = 1;
+          }
+        } if (game_data.input & B_BTN) {
+          if (e_bits_left < 2) done = 1;
+        } if (game_data.input & U_BTN) {
+          if (curs_row > 2) curs_row -= 1;
+        } if (game_data.input & D_BTN) {
+          if (curs_row < 5) curs_row += 1;
+        } if (game_data.input & R_BTN) {
+          if (curs_col > 2) curs_col -= 1;
+        } if (game_data.input & L_BTN) {
+          if (curs_col < 5) curs_col += 1;
+        }
+      } else {
+        eq_count = (eq_count == 500) ? 0 : eq_count + 1;
+      }
+
+      disp.control(MD_MAX72XX::UPDATE, MD_MAX72XX::OFF);
+      disp.clear();
+      for (int i = 0; i < GAME_HEIGHT; i += 1) {
+        if (!done) disp.setRow(2, i, (uint8_t)(0x55 >> (i % 2)));
+        disp.setRow(1, i, create_frame[i]);
+        disp.setRow(1, curs_row, (uint8_t)(1 << curs_col) | 0x42 | create_frame[curs_row]);
+        if (!done) disp.setRow(0, i, (uint8_t)(0x55 >> (i % 2)));      
+      }
+      disp.control(MD_MAX72XX::UPDATE, MD_MAX72XX::ON);
+      prev_input = game_data.input;
     }
-    disp.control(MD_MAX72XX::UPDATE, MD_MAX72XX::ON);
-    prev_input = game_data.input;
   }
+  
 
   delay(2000);
+
+  quip.say(spt_READY_TO_START);
+  delay(1000);
+  quip.say(spa_THREE);
+  delay(500);
+  quip.say(spa_TWO);
+  delay(500);
+  quip.say(spa_ONE);
+  delay(500);
+  quip.say(spt_GO);
+
+
   player.sprite = sprite;
   player.emitter = emitter;
+}
+
+void game_over_animation() {
+  uint8_t done = 0, num_empty = 0;
+  while (!done) {
+    for (int i = 0; i < GAME_HEIGHT; i += 1) {
+  
+  
+      if (i % 2 == 0) game_data.frame[i] >>= 1;
+      else game_data.frame[i] <<= 1;
+      delay(10);
+      
+      push_frame();
+    }
+
+    for (int i = 0; i < GAME_HEIGHT; i += 1) {
+      if (!game_data.frame[i]) num_empty += 1;
+    }
+
+    if (num_empty == GAME_HEIGHT) done = 1;
+    else num_empty = 0;
+  }
+  quip.say(spt_PLAY);
+  quip.say(spa_AGAIN);
+  quip.say(spt_IF);
+  quip.say(spt_YOU);
+  quip.say(DARE);
 }
 
 void update_game() {
@@ -149,22 +197,17 @@ void update_input() {
       // atk cooldown check
       if (player.atk_timer.c_time >= player.atk_timer.interval) {
         // base attack
-        player_attack(0);
+        player_attack();
+        enemy_attack();
         // reset cd
         player.atk_timer.i_time = millis();
-      }
-    } if (game_data.input & B_BTN) {
-      // super cooldown check
-      if (player.super_timer.c_time >= player.super_timer.interval) {
-        // super attack (needs robust game states for stop-time effect)
-        player_attack(1);   // needs other testing before full implementaiton 
-      }      
+      }    
     } if (game_data.input & U_BTN) {
       player.r_pos = (player.r_pos > 0 ? player.r_pos - 1 : 0);
     } if (game_data.input & D_BTN) {
-      player.r_pos = (player.r_pos + player.s_height < GAME_HEIGHT ? player.r_pos + 1 : GAME_HEIGHT - player.s_height);
+      player.r_pos = (player.r_pos + player.s_height < GAME_HEIGHT ? player.r_pos + 1 : GAME_HEIGHT - player.s_height);     
     } if (game_data.input & R_BTN) {
-      player.c_pos = (player.c_pos < GAME_WIDTH - 1 ? player.c_pos + 1 : GAME_WIDTH);
+      player.c_pos = (player.c_pos < GAME_WIDTH - 1 ? player.c_pos + 1 : GAME_WIDTH);    
     } if (game_data.input & L_BTN) {
       player.c_pos = (player.c_pos > 0 ? player.c_pos - 1 : 0);
     }
@@ -176,22 +219,17 @@ void update_input() {
 }
 
 void update_player() {
-  static uint32_t sprite_cache = 0;
   uint32_t p_slice = 0, e_slice = 0;
 
   player.atk_timer.c_time = millis() - player.atk_timer.i_time;
-  player.super_timer.c_time = millis() - player.super_timer.i_time;
 
-  // recalculates sprite height if sprite deformation is detected
-  if (sprite_cache != player.sprite) {
-    player.s_height = 0;
-    for (int i = 0; i < SPRITE_HEIGHT; i += 1) {
-      if (player.sprite & (0x000F << (i * SPRITE_WIDTH))) player.s_height += 1;
-    }
+  player.s_height = 0;
+  for (int i = 0; i < SPRITE_HEIGHT; i += 1) {
+    if (player.sprite & (0x000F << (i * SPRITE_WIDTH))) player.s_height += 1;
+  }
 
-    if (!(player.sprite & 0xF000)) {
-      player.sprite <<= SPRITE_WIDTH;
-    }
+  if (!(player.sprite & 0xF000)) {
+    player.sprite <<= SPRITE_WIDTH;
   }
 
   for (int i = 0; i < GAME_HEIGHT; i += 1) {
@@ -227,8 +265,6 @@ void update_player() {
       }
     }
   }
-
-  sprite_cache = player.sprite;
 }
 
 void update_enemies() {
@@ -243,14 +279,7 @@ void update_enemies() {
       uint8_t e_depth = i - enemies[k].r_pos;
       if (e_depth >= 0 && e_depth < SPRITE_HEIGHT) {  /* if "scanline" hits enemy */
         if (enemies[k].atk_timer.c_time >= enemies[k].atk_timer.interval) { /* fire projectile */
-          uint32_t em_slice = get_sprite_slice(e_depth, enemies[k].emitter);
-          em_slice <<= GAME_WIDTH - enemies[k].c_pos - SPRITE_WIDTH;
-
-          // fire projectile
-          game_data.e_proj[i] |= em_slice;
-
-          // reset atk timer
-          enemies[k].atk_timer.i_time = millis();
+          enemy_attack();
         }
 
         e_slice = get_sprite_slice(e_depth, enemies[k].sprite);
@@ -302,23 +331,34 @@ void update_projectiles() {
 
 void update_conditions() {
   if (player.sprite == 0) {
-    game_data.g_state |= GAME_OVER;
+    game_over();
   }
 
   uint8_t alive_ct = game_data.num_enemies;
   for (int i = 0; i < game_data.num_enemies; i += 1) {
     if (enemies[i].sprite == 0) {
-        alive_ct -= 1;
+      alive_ct -= 1;
+    } if (enemies[i].c_pos == 0) {
+      game_over();
     }
   }
 
   if (alive_ct == 0) {
-    game_data.g_state = GAME_OVER; 
+    if (game_data.cur_level == NUM_LEVELS) {
+      game_data.g_state = GAME_OVER;
+      quip.say(spt_YOU_WIN);
+      game_over_animation();
+    } else {
+      game_data.g_state = GAME_OVER;
+      game_data.cur_level += 1;
+      quip.say(spt_NEXT);
+      quip.say(spt_ROUND);
+    }
   }
 }
 
 void update_frame() {
-  uint8_t p_depth, e_depth;
+  uint8_t p_depth, e_depth, su_depth;
   for (int i = 0; i < GAME_HEIGHT; i += 1) {
     game_data.frame[i] = 0;
 
@@ -347,19 +387,57 @@ void update_frame() {
   }
 }
 
-void player_attack(uint8_t super) {
-  if (super) {
-    // leave for now
-  } else {
-    for (int i = 0; i < GAME_HEIGHT; i += 1) {
-      uint8_t p_depth = i - player.r_pos;
-      if (p_depth >= 0 && p_depth < player.s_height) {
-        uint32_t em_slice = get_sprite_slice(p_depth, player.emitter);
-        em_slice <<= GAME_WIDTH - player.c_pos - SPRITE_WIDTH;
-        game_data.p_proj[i] |= em_slice;
-      }
+void player_attack() {
+  for (int i = 0; i < GAME_HEIGHT; i += 1) {
+    uint8_t p_depth = i - player.r_pos;
+    if (p_depth >= 0 && p_depth < player.s_height) {
+      uint32_t em_slice = get_sprite_slice(p_depth, player.emitter);
+      em_slice <<= GAME_WIDTH - player.c_pos - SPRITE_WIDTH;
+      game_data.p_proj[i] |= em_slice;
     }
-  } 
+  }
+}
+
+void enemy_attack() {
+  for (int k = 0; k < game_data.num_enemies; k += 1) {
+    for (int i = 0; i < GAME_HEIGHT; i += 1) {
+      uint8_t e_depth = i - enemies[k].r_pos;
+      uint32_t em_slice = get_sprite_slice(e_depth, enemies[k].emitter);
+      em_slice <<= GAME_WIDTH - enemies[k].c_pos - SPRITE_WIDTH;
+
+      // fire projectile
+      game_data.e_proj[i] |= em_slice;
+
+      enemies[k].atk_timer.i_time = millis();
+    }
+  }
+}
+
+void game_over() {
+  game_data.g_state = GAME_OVER;
+    
+  quip.say(pacman_death);
+
+  randomSeed(analogRead(0));
+  uint16_t rand = random(0,100);
+  if (rand < 25) {
+    quip.say(sp4_MAYDAY);
+  } else if (rand >= 25 && rand < 50) {
+    quip.say(spt_TRY_AGAIN);
+  } else if (rand >= 50 && rand < 75) {
+    quip.say(sp2_POWER);
+    quip.say(spa_DOWN);
+  } else if (rand >= 75 && rand < 98) {
+    quip.say(spa_ERROR);
+    quip.say(spa_ERROR);
+  } else if (rand >= 98) {
+    quip.say(sp2_QUEBEC);
+  }
+
+  quip.say(spt_GAMES);
+  quip.say(spt_OVER);
+
+  game_over_animation();
 }
 
 void push_frame() {
